ea6917e50d6c4a95b25fd3c960153099aa108b40
csbonang <csgbonang@gmail.com>
2021-08-13 10:33:16 -0700

    activity2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-// First we import our Hello component from our components folder.
-import Hello from './components/Hello';
+// TODO: Add a comment explaining what this import statement is doing
+// bringing in the components file 
+import HelloReact from './components/HelloReact';
 
-// React apps typically have a single App component at the very top that can reference other React components.
-// This component, `App`, is our main component that is importing `Hello` and rendering it in the return method.
+// TODO: Add a comment explaining the purpose of the App component
+
 function App() {
-  return <Hello />;
+  return <HelloReact />;
 }
 
+// just exports the app component so that the other file can use it 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+// TODO: Add a comment giving a brief description of what React is
 import React from 'react';
+
+// TODO: Add a comment that describes the difference between react and react-dom
+// allows us to use the react-dom
 import ReactDOM from 'react-dom';
+// brings in the app.js file 
 import App from './App';
 
-// The ReactDOM.render method is used to render a react element into the actual DOM
-// The first argument is the component we want to render, and the second is the container element on the page
+// TODO: Add a comment describing the significance of the ReactDOM.render method
+// Render a React element into the DOM in the supplied container and return a reference to the component
 ReactDOM.render(<App />, document.getElementById('root'));
